7e47b9868807e4a74fbd0bf52a370597176ca501
MtheEPIC <mtheepic1423@protonmail.com>
2021-05-03 01:27:50 +0300

    player functions

--- Player.hpp
+++ Player.hpp
@@ -1,10 +1,24 @@
 #pragma once
+#include <string>
+#include "Board.hpp"
+#include "City.hpp"
 
 namespace pandemic {
 	// typedef unsigned int UI;
 	class Player
 	{
 	public:
-		Player();
+		Player(Board, City);
+		Player take_card(City);
+		Player drive(City);
+		Player fly_direct(City);
+		Player treat(City);
+		Player fly_charter(City);
+		
+		Player build();
+		Player fly_shuttle(City);
+		Player discover_cure(Color);
+		std::string role();
 	};
+	std::ostream& operator<<(std::ostream& os, const Board& dt);
 }
