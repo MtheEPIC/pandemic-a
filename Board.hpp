32667011eedf038464071774db202c02dd210c36
MtheEPIC <mtheepic1423@protonmail.com>
2021-05-03 22:06:33 +0300

    board overloads

--- Board.hpp
+++ Board.hpp
@@ -1,14 +1,17 @@
 #pragma once
 #include "City.hpp"
 #include <iostream>
+#include <unordered_map>
 
 namespace pandemic {
+	typedef std::unordered_map<City, int> M;
 	class Board
 	{
+		M cities_pandemic_level;
 	public:
-		Board operator[](City);
-		Board operator=(int);
+		int& operator[](City);
 		bool is_clean();
 	};
 	std::ostream& operator<<(std::ostream& os, const Board& dt);
+	
 }
