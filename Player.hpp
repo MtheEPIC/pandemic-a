e65fa4ed635f5e983daa4fcca6f3c84a112f70ee
MtheEPIC <mtheepic1423@protonmail.com>
2021-05-03 22:07:45 +0300

    player structure

--- Player.hpp
+++ Player.hpp
@@ -2,19 +2,20 @@
 #include <string>
 #include "Board.hpp"
 #include "City.hpp"
+#include "Color.hpp"
 
 namespace pandemic {
-	// typedef unsigned int UI;
 	class Player
 	{
 	public:
 		Player(Board, City);
+		Player(Board, City, int);
+		~Player();
 		Player take_card(City);
 		Player drive(City);
 		Player fly_direct(City);
 		Player treat(City);
 		Player fly_charter(City);
-		
 		Player build();
 		Player fly_shuttle(City);
 		Player discover_cure(Color);
